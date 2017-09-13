f7282350212fbad08decd0a7505492f1c1482f8e
unknown <rolandojanet2012@gmail.com>
2017-09-13 16:42:17 -0400

    Add the templates into the website, ContactUs, WhyUs, GetStarted, Homepage

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,7 +15,64 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+           
         ]);
     }
+	
+	/**
+     * @Route("/why-us", name="why-us")
+     */
+    public function whyusAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('why-us/why-us.html.twig', [
+            
+        ]);
+    }
+	
+	/**
+     * @Route("/contact-us", name="contact-us")
+     */
+    public function contactusAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('contact-us/contact-us.html.twig', [
+           
+        ]);
+    }
+	
+	/**
+     * @Route("/get-started", name="get-started")
+     */
+    public function getstartedAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('get-started/getstarted.html.twig', [
+            
+        ]);
+    }
+	
+	/**
+     * @Route("/power-ups", name="power-ups")
+     */
+    public function powerupsAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('power-ups/power-ups.html.twig', [
+            
+        ]);
+    }
+	
+	/**
+     * @Route("/warm-leads", name="warm-leads")
+     */
+    public function warmleadsAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('warm-leads/warm-leads.html.twig', [
+            
+        ]);
+    }
+	
+	
 }
